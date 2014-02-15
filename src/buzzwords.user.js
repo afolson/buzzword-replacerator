df0153b2fcb1e81da6cb95e692d05c90b35e74c7
Louis T <LouisT@ltdev.im>
2014-02-14 18:57:12 -0800

    increase the version number, add download and update url meta data

--- src/buzzwords.user.js
+++ src/buzzwords.user.js
@@ -2,11 +2,13 @@
 // @name        Buzzword Replacerator
 // @namespace   https://github.com/afolson/buzzword-replacerator
 // @description A UserScript port of the Google Chrome extension to replace buzzwords with more accurate words.
-// @version     0.1.1
+// @version     0.1.2
 // @grant       none
 // @include     *
 // @match       http://*/*
 // @match       https://*/*
+// @downloadURL https://github.com/afolson/buzzword-replacerator/raw/userscript/src/buzzwords.user.js
+// @updateURL   https://github.com/afolson/buzzword-replacerator/raw/userscript/src/buzzwords.user.js
 // ==/UserScript==
 // buzzword-replacerator - Replace common buzzwords with more accurate descriptions.
 // Copyright (C) 2013  Amanda Folson <amanda@incredibl.org>
